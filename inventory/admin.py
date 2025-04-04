7e2c50f58b8a46b9ea9c3dc3a8b15476691548ec
Kiri-Sigh <Sigh.tul.1@gmail.com>
2025-04-05 00:43:44 +0700

    hmm

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from .models import (
+    ComponentCategory, CheapItem, ExpensiveItem, ExpensiveItemData, BorrowItemList
+)
 
-# Register your models here.
+@admin.register(ComponentCategory)
+class ComponentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'category')
+    search_fields = ('category',)
+    ordering = ('category',)
+
+
+@admin.register(CheapItem)
+class CheapItemAdmin(admin.ModelAdmin):
+    list_display = ('component_id', 'name', 'category', 'stock', 'component_status', 'requires_admin_approval')
+    list_filter = ('category', 'component_status', 'requires_admin_approval')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    readonly_fields = ('component_id', 'image')
+    fieldsets = (
+        ('Basic Info', {'fields': ('name', 'category', 'description', 'image')}),
+        ('Stock & Availability', {'fields': ('stock', 'component_status', 'amount_reserved_rn', 'amount_reserve')}),
+        ('Time & Weight', {'fields': ('max_time', 'weight')}),
+        ('Permissions', {'fields': ('requires_admin_approval',)})
+    )
+
+
+@admin.register(ExpensiveItem)
+class ExpensiveItemAdmin(admin.ModelAdmin):
+    list_display = ('component_id', 'name', 'category', 'component_status', 'requires_admin_approval', 'late_penalty')
+    list_filter = ('category', 'component_status', 'requires_admin_approval')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    readonly_fields = ('component_id', 'image')
+    fieldsets = (
+        ('Basic Info', {'fields': ('name', 'category', 'description', 'image')}),
+        ('Stock & Availability', {'fields': ('component_status', 'amount_reserved_rn', 'amount_reserve')}),
+        ('Time & Weight', {'fields': ('max_time', 'weight', 'change_hands_interval')}),
+        ('Penalty & Permissions', {'fields': ('late_penalty', 'requires_admin_approval')}),
+    )
+
+
+@admin.register(ExpensiveItemData)
+class ExpensiveItemDataAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'expensive_item', 'user', 'serial_id', 'stock', 'item_status', 'reserved', 'force_reserved')
+    list_filter = ('item_status', 'reserved', 'force_reserved')
+    search_fields = ('serial_id', 'expensive_item__name', 'user__username')
+    ordering = ('serial_id',)
+    readonly_fields = ('item_id', 'image')
+    fieldsets = (
+        ('Item Details', {'fields': ('expensive_item', 'serial_id', 'image')}),
+        ('User & Status', {'fields': ('user', 'item_status', 'reserved', 'force_reserved')}),
+        ('Stock & Condition', {'fields': ('stock', 'weight', 'condition')}),
+        ('Time & Penalty', {'fields': ('max_time', 'late_penalty', 'change_hands_interval')}),
+        ('Approval', {'fields': ('requires_admin_approval',)}),
+    )
+
+
+@admin.register(BorrowItemList)
+class BorrowItemListAdmin(admin.ModelAdmin):
+    list_display = ('borrow_id', 'user', 'expensive_item_data', 'cheap_item', 'quantity_specified', 'date_start', 'date_end')
+    list_filter = ('quantity_specified', 'date_specified')
+    search_fields = ('user__username', 'expensive_item_data__serial_id', 'cheap_item__name')
+    ordering = ('date_start',)
+    fieldsets = (('item in locker ready',{'fields': ('item_in_locker_done',)})
+                 ,('item returned',{'fields':('item_returned')}),
+        ('Borrower Details', {'fields': ('user',)}),
+        ('Items Borrowed', {'fields': ('expensive_item_data', 'cheap_item')}),
+        ('Quantity & Date', {'fields': ('quantity_specified', 'quantity', 'date_specified', 'date_start', 'date_end')}),
+    )
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "profile_picture")}),
+        (_("Enrollment Details"), {"fields": ("enrolled_year", "enrolled_department", "merit", "level")}),
+        (_("Authentication"), {"fields": ("google_id",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "password1", "password2"),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.changed_data:  # Only hash password if changed
+            obj.set_password(obj.password)
+        obj.save()
+
+admin.site.register(CustomUser, CustomUserAdmin)
